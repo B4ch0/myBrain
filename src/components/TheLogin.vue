155a91270974b820231d168b8cbc67caa9abf1d3
B4ch0 <danielbacac@gmail.com>
2023-03-15 17:07:44 +0100

    Login clenaup V1

--- src/components/TheLogin.vue
+++ src/components/TheLogin.vue
@@ -13,9 +13,10 @@ const count = ref(0)
   <div class="flex-column centered">
     <h1>Login</h1>
     <form>
-        <input v-model="email" placeholder="email" />
-        <input v-model="password" placeholder="password" type="password"/>
+        <input v-model="email" placeholder="Email" />
+        <input v-model="password" placeholder="Password" type="password"/>
         <p>Submit</p>
+        <p>Create new account: </p>
       </form>
   </div>
 
@@ -25,5 +26,26 @@ const count = ref(0)
 </template>
 
 <style scoped>
+.centered{
+  margin: 0 auto;
+  width: 300px;
+}
 
+input{
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 150%;
+  border: 1px solid #E1E1E1;
+  background: #fff;
+  color: #9F9F9F;
+  padding: 12px 40px 12px 20px;
+  margin-bottom: 20px;
+  min-width: 100%;
+  box-sizing:border-box;
+}
+
+input:focus {
+  border-color: #B5B5B5;
+  color: #343030;
+}
 </style>
