67c13f26572f54c5dd61814ba640be838c0ff46b
B4ch0 <danielbacac@gmail.com>
2023-03-15 16:34:00 +0100

    Login screen

---
+++ src/components/MainScreen.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div class="container">
+    <div class="flex-row mainScreen">
+
+    </div>
+  </div>
+</template>
+
+
+
+
+
+
+
+
+<style scoped>
+.mainScreen{
+  height: 100vh;
+}
+
+</style>
---
+++ src/components/TheLogin.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+<div class="container">
+  <div class="flex-column centered">
+    <h1>Login</h1>
+    <form>
+        <input v-model="email" placeholder="email" />
+        <input v-model="password" placeholder="password" type="password"/>
+        <p>Submit</p>
+      </form>
+  </div>
+
+
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
