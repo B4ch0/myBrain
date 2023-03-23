635cc44f14885957a76ecb7652e9c0f6d3746b5a
B4ch0 <danielbacac@gmail.com>
2023-03-23 14:47:43 +0100

    + notebooks

--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -1,23 +1,40 @@
 <script setup>
 import { useRouter } from 'vue-router'
 import { signOut } from "firebase/auth";
-import { useFirebaseAuth, useCurrentUser } from 'vuefire'
+import { useFirebaseAuth, useCurrentUser, useFirestore } from 'vuefire'
 const auth = useFirebaseAuth();
 
 const router = useRouter();
 const user = useCurrentUser();
-
+const db = useFirestore()
 function logOut(){
   signOut(auth).then(() => { router.push('/login')})}
-     
+ 
+async function createNotebook(){
+  const name = prompt("Create notebook", "New note");
+  await addDoc(collection(db, "notebooks"), {
+    user: user.value.uid,
+    name: name,
+    notes: [],
+    children: []
+  });
+  notebookName.value = ""
+}
     
 </script>
 <template>
    <div class="container">
     <div class="flex-row mainScreen">
       <div class="flex-column menu">
-       <p >User: <strong>{{ user.mail }}</strong> / <a href="#" @click="logOut()">Log Out</a></p>
+        <template v-if="user"><div>
+       <p >User: <strong>{{ user.mail }}</strong> / <a href="#" @click="logOut()">Log Out</a></p></div>
+      </template>
+      <div class="flex-row title-menu">
+        <h2>Notebooks</h2>
+        <button class="addIcon" @click="createNotebook()" >+</button>
       </div>
+    
+    </div>
     </div>
   </div>
 </template>
@@ -42,5 +59,21 @@ function logOut(){
 }
 .menu a{
   color: #fff;
+}
+
+
+.title-menu{
+  position: relative;
+}
+.title-menu h2{
+  text-transform: uppercase;
+  font-weight: bold;
+}
+.addIcon{
+  position: absolute;
+  top: 10px;
+  right: 0;
+  font-size: 30px;
+  font-weight: 700;
 }
 </style>
